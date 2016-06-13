47b57026248e69dc5a38884e1894c46045294adf
csf30816 <elijahporter@zoho.com>
2016-06-12 20:02:36 -0500

    Update index.php

--- login/index.php
+++ login/index.php
@@ -1,6 +1,6 @@
 <?php // index.php
 require_once 'openid.php';
-$openid = new LightOpenID("csf30816.github.io/Snapmod");
+$openid = new LightOpenID("csf30816.github.io/Snapmod/login");
 
 $openid->identity = 'https://www.google.com/accounts/o8/id';
 $openid->required = array(
@@ -8,7 +8,7 @@ $openid->required = array(
   'namePerson/last',
   'contact/email',
 );
-$openid->returnUrl = 'http://csf30816.github.io/Snapmod/login.php'
+$openid->returnUrl = 'http://csf30816.github.io/Snapmod/login/login.php'
 ?>
 
 <a href="<?php echo $openid->authUrl() ?>">Login with Google</a>
